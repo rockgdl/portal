00173537aacce812b429ec3be999a4779079e5b6
fabian <fpiceno@urrea.com.mx>
2018-11-27 17:20:07 -0600

    session

--- webPortal/src/main/java/com/fpiceno/webproduct/dao/sql/ProductoDaoSql.java
+++ webPortal/src/main/java/com/fpiceno/webproduct/dao/sql/ProductoDaoSql.java
@@ -3,6 +3,8 @@ package com.fpiceno.webproduct.dao.sql;
 import java.util.List;
 
 import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
 
 import com.fpiceno.webproduct.dao.ProductoDao;
 import com.fpiceno.webproduct.dto.Producto;
@@ -47,8 +49,18 @@ public class ProductoDaoSql implements ProductoDao {
 
 	@Override
 	public Session getSession() {
-		// TODO Auto-generated method stub
-		return null;
+//		 Transaction tx;
+		 Session sesion = null;
+		 SessionFactory sessionFactoryTest=null;
+		 sessionFactoryTest= new Configuration().configure("com/fpiceno/webproduct/configuracion/hibernateSql.cfg.xml").buildSessionFactory();
+//			sessionFactory=new Configuration().configure(_archivo).buildSessionFactory();
+//			LOG.info("TEST UNITARIOS SI DA ESTE ERROR HAY QUE CORREGIR EL OBJETO DEL DAO ");
+		 sesion = sessionFactoryTest.getCurrentSession();
+//		 tx = sesion.beginTransaction();
+//		 tx.commit();
+		
+			
+			return sesion;
 	}
 
 }
